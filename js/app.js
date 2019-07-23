c42d04b4867a40092c649bc6cea741016c33da11
Csorba László <csorbalaci@gmail.com>
2019-07-23 13:58:51 +0200

    validation

--- js/app.js
+++ js/app.js
@@ -1,19 +1,34 @@
+var dateTimeHolder = document.getElementById('datetime');
 var balanceHolder = document.getElementById('balance');
 var amountHolder = document.getElementById('amount');
 var storeButton = document.getElementById('store');
 var transactions = [];
 
 storeButton.addEventListener('click', function addAmount() {
-    var intValue = parseInt(amountHolder.value);
-    transactions.push(intValue);
-    console.log(transactions);
-    amountHolder.value = '';
-    balanceHolder.textContent = calculateBalance();
+    if (isTransactionValid(amountHolder, dateTimeHolder)) {
+        var intValue = parseInt(amountHolder.value);
+        transactions.push(intValue);
+        console.log(transactions);
+        amountHolder.value = '';
+        balanceHolder.textContent = calculateBalance();
+    }
 })
+
+function isTransactionValid(amountHolder, dateTimeHolder) {
+    var amountValid = true;
+    var datetimeValid = true;
+    if (isNaN(parseInt(amountHolder.value))) {
+        amountValid = false;
+        amountHolder.classList.add('invalid');
+    } else {
+        amountHolder.classList.remove('invalid');
+    }
+    return amountValid && datetimeValid;
+}
 
 function calculateBalance() {
     var balance = 0;
-    for(var i = 0; i < transactions.length; i++) {
+    for (var i = 0; i < transactions.length; i++) {
         balance += transactions[i];
     }
     return balance;
