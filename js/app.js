2f60d61fcbb31a34f8c9624c6134d47af8ec0299
Csorba László <csorbalaci@gmail.com>
2019-07-26 15:52:44 +0200

    callback

--- js/app.js
+++ js/app.js
@@ -4,11 +4,31 @@ var amountHolder = document.getElementById('amount');
 var storeButton = document.getElementById('store');
 var transactionsListContainer = document.getElementById("transactions");
 var transactionRowTemplate = document.querySelector('.transactions-row-template');
-var transactions = getTransactions();
-transactions.forEach(function(transaction, index) {
-insertTransactionRow(transaction.amount, new Date(transaction.date), index);
+
+function getTransaction(id, callback) {
+    console.log("Getting transaction #" + id + ".")
+    callback(id);
+}
+
+getTransaction(5, function (id) {
+    logTransaction(id, function reallyLogTransaction(id) {
+        console.log("Transactions #" + id + " seriously received.");
+    });
 });
 
+function logTransaction(id, callback) {
+    console.log("Transactions #" + id + " retrieved.")
+    callback(id);
+}
+
+function reallyLogTransaction(id) {
+    console.log("Transactions #" + id + " seriously received.")
+}
+
+var transactions = getTransactions();
+transactions.forEach(function (transaction, index) {
+    insertTransactionRow(transaction.amount, new Date(transaction.date), index);
+});
 storeButton.addEventListener('click', function addAmount() {
     if (isTransactionValid(amountHolder, dateTimeHolder)) {
         var intValue = parseInt(amountHolder.value);
@@ -34,59 +54,59 @@ function saveTransaction(amount, date) {
     localStorage.setItem("transactions", transactionsString)
 }
 
-    function insertTransactionRow(amount, date, index) {
-        var listItem = transactionRowTemplate.cloneNode(true);
-        listItem.querySelector('.transaction-id').textContent = index;
-        listItem.querySelector('.transaction-date').textContent = date.toDateString();
-        listItem.querySelector('.transaction-amount').textContent = amount + " Ft";
-        var deleteButton = listItem.querySelector('.delete-transaction');
-        deleteButton.setAttribute('data-id', index)
-        deleteButton.addEventListener('click', deleteTransactionRow);
-        transactionsListContainer.appendChild(listItem);
-        balanceHolder.textContent = calculateBalance();
-    }
+function insertTransactionRow(amount, date, index) {
+    var listItem = transactionRowTemplate.cloneNode(true);
+    listItem.querySelector('.transaction-id').textContent = index;
+    listItem.querySelector('.transaction-date').textContent = date.toDateString();
+    listItem.querySelector('.transaction-amount').textContent = amount + " Ft";
+    var deleteButton = listItem.querySelector('.delete-transaction');
+    deleteButton.setAttribute('data-id', index)
+    deleteButton.addEventListener('click', deleteTransactionRow);
+    transactionsListContainer.appendChild(listItem);
+    balanceHolder.textContent = calculateBalance();
+}
 
-    function deleteTransactionRow(event) {
-        var id = event.target.getAttribute('data-id');
-        transactions.splice(id, 1);
-        var row = event.target.parentNode.parentNode;
-        row.parentNode.removeChild(row);
-        balanceHolder.textContent = calculateBalance();
-    }
+function deleteTransactionRow(event) {
+    var id = event.target.getAttribute('data-id');
+    transactions.splice(id, 1);
+    var row = event.target.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+    balanceHolder.textContent = calculateBalance();
+}
 
-    function isTransactionValid(amountHolder, dateTimeHolder) {
-        var amountValid = validateAmount(amountHolder);
-        var datetimeValid = validateDate(dateTimeHolder);
-        return amountValid && datetimeValid;
-    }
+function isTransactionValid(amountHolder, dateTimeHolder) {
+    var amountValid = validateAmount(amountHolder);
+    var datetimeValid = validateDate(dateTimeHolder);
+    return amountValid && datetimeValid;
+}
 
-    function validateDate(dateTimeHolder) {
-        var datetimeValid = true;
-        var date = new Date(dateTimeHolder.value);
-        if (date.toString() === "Invalid Date") {
-            datetimeValid = false;
-            dateTimeHolder.classList.add('invalid');
-        } else {
-            dateTimeHolder.classList.remove('invalid');
-        }
-        return datetimeValid;
+function validateDate(dateTimeHolder) {
+    var datetimeValid = true;
+    var date = new Date(dateTimeHolder.value);
+    if (date.toString() === "Invalid Date") {
+        datetimeValid = false;
+        dateTimeHolder.classList.add('invalid');
+    } else {
+        dateTimeHolder.classList.remove('invalid');
     }
+    return datetimeValid;
+}
 
-    function validateAmount(amountHolder) {
-        var amountValid = true;
-        if (isNaN(parseInt(amountHolder.value))) {
-            amountValid = false;
-            amountHolder.classList.add('invalid');
-        } else {
-            amountHolder.classList.remove('invalid');
-        }
-        return amountValid;
+function validateAmount(amountHolder) {
+    var amountValid = true;
+    if (isNaN(parseInt(amountHolder.value))) {
+        amountValid = false;
+        amountHolder.classList.add('invalid');
+    } else {
+        amountHolder.classList.remove('invalid');
     }
+    return amountValid;
+}
 
-    function calculateBalance() {
-        var balance = 0;
-        for (var i = 0; i < transactions.length; i++) {
-            balance += transactions[i].amount;
-        }
-        return balance + " Ft";
+function calculateBalance() {
+    var balance = 0;
+    for (var i = 0; i < transactions.length; i++) {
+        balance += transactions[i].amount;
     }
+    return balance + " Ft";
+}
